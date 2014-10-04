4ca1ac31f27d33821fe84b4fd23f723d966396c6
Matt Farmer <matt@frmr.me>
2014-10-04 11:19:09 -0400

    Some Scaladoc, yo.

--- src/main/scala/me/frmr/rubyist/RubySnippetRunner.scala
+++ src/main/scala/me/frmr/rubyist/RubySnippetRunner.scala
@@ -10,12 +10,45 @@ import net.liftweb._
 
 import org.jruby.embed._
 
+/**
+ * The RubySnippetRunner allows you to run a Ruby script and retrieve a
+ * (NodeSeq)=>NodeSeq function that can be executed like any other Lift
+ * snippet method.
+ *
+ * The constructor takes two parameters: a modulesPath from which ruby
+ * scripts will be loaded, and a ScriptingContainer that defines the
+ * ScriptingContainer object you want to use to execute the scripts with.
+ *
+ * The modulesPath will be read from the Java prop modules.path by default
+ * and the ScriptingContainer just uses the default settings. As a part of
+ * instantiation, the ClassLoader for the ScriptingContainer will be set to
+ * this class's ClassLoader, as the default one seems to lack access to the
+ * neccicary Scala components. This means that, for now, you shouldn't
+ * allow untrusted users to define snippets. They could do mischevious things.
+ *
+ * @param modulesPath The path from which to load modules.
+ * @param container The ScriptingContainer inside of which ruby scripts will execute.
+**/
 class RubySnippetRunner(
   modulesPath: String = Props.get("modules.path").openOr(""),
   container: ScriptingContainer = new ScriptingContainer
 ) extends Loggable {
   container.setClassLoader(getClass.getClassLoader)
 
+  /**
+   * Evaluate a ruby snippet, returning the result. This method will resolve the
+   * snippet path using the format `modulesPath/moduleName/snippetName`. So, if
+   * you have a module named "posts" and a ruby snippet named "view" you would
+   * execute something like `evaluate("posts", "view")` and this method would
+   * load `<modulesPath>/posts/view.rb`.
+   *
+   * This method will pull in and evaluate the defined base.rb that sets up some
+   * required helpers for CSS transforms to work in Ruby land.
+   *
+   * @param moduleName The name of the module that the Ruby Snippet resides in.
+   * @param snippetName The name of the ruby snippet, minus the ".rb" on the end.
+   * @return A (NodeSeq)=>NodeSeq.
+  **/
   def evaluate(moduleName: String, snippetName: String): (NodeSeq)=>NodeSeq = {
     val moduleFilename = s"$modulesPath/$moduleName/$snippetName.rb"
 
